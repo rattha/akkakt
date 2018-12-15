c0c4889634d29a77e1690c10552dd6e7b009e762
rattha <rattha_p@yahoo.com>
2018-12-16 02:18:25 +0700

    stream to elasticsearch

--- src/main/kotlin/blog/actor/MyActorToStream.kt
+++ src/main/kotlin/blog/actor/MyActorToStream.kt
@@ -6,9 +6,16 @@ import akka.actor.ActorSystem
 import akka.japi.pf.ReceiveBuilder
 import akka.stream.ActorMaterializer
 import akka.stream.OverflowStrategy
+import akka.stream.alpakka.elasticsearch.ElasticsearchWriteSettings
+import akka.stream.alpakka.elasticsearch.WriteMessage
+import akka.stream.alpakka.elasticsearch.javadsl.ElasticsearchFlow
+import akka.stream.alpakka.elasticsearch.javadsl.ElasticsearchSink
 import akka.stream.javadsl.Sink
 import akka.stream.javadsl.Source
-import org.springframework.context.annotation.Lazy
+import blog.data.MyEvent
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.http.HttpHost
+import org.elasticsearch.client.RestClient
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 
@@ -19,12 +26,34 @@ class MyActorToStream(system: ActorSystem) : AbstractActor() {
     var actStream: Source<String, ActorRef>? = null
     var a: ActorRef? = null
 
+    val client = RestClient.builder(HttpHost("localhost", 9200)).build()
+
+    var count = 0
+
     init {
         val mat = ActorMaterializer.create(system)
 
         actStream = Source.actorRef<String>(100, OverflowStrategy.fail())
 
-        a = actStream?.to(Sink.foreach { println(it) })?.run(mat)
+        val sink = ElasticsearchSink.create<Any>(
+                "sink3",
+                "_doc",
+                ElasticsearchWriteSettings.create().withBufferSize(5),
+                client,
+                ObjectMapper())
+
+        a = actStream
+                ?.map { _ -> count++ }
+                ?.map { m -> WriteMessage.createIndexMessage(m.toString(), MyEvent("TEST")) }
+                ?.via(
+                        ElasticsearchFlow.create(
+                                "sink3",
+                                "_doc",
+                                ElasticsearchWriteSettings.create().withBufferSize(5),
+                                client,
+                                ObjectMapper()))
+                ?.to(Sink.foreach { println(it) })?.run(mat)
+
     }
 
     override fun createReceive() = ReceiveBuilder().match(String::class.java) {
---
+++ src/main/kotlin/blog/data/MyEvent.kt
@@ -0,0 +1,3 @@
+package blog.data
+
+data class MyEvent(val name: String)
